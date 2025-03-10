d445fe1c399a2d564f231550108f26269ef9b7e3
AkashDeore15 <akashdeore1999@gmail.com>
2025-03-09 23:35:20 -0400

    pytest

--- src/logger.py
+++ src/logger.py
@@ -1,3 +1,4 @@
+""" This module configures the logging for the application. """
 import logging
 import os
 
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,6 +1,8 @@
+""" Test the logger module """
 import os
 from src.logger import logger
 
 def test_logger():
+    """ Test the logger module """
     os.environ["LOG_LEVEL"] = "DEBUG"
     assert logger is not None
